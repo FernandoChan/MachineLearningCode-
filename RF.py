4fb08c6f988bd4d25e0db7e2dddedbbecae515b4
Fernando <m15521106787@gmail.com>
2019-05-17 00:56:18 +0800

    添加其他的训练方法

--- RF.py
+++ RF.py
@@ -1,8 +1,12 @@
 import  numpy as np
-from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.ensemble import RandomForestClassifier as RFC , GradientBoostingClassifier as GBC, AdaBoostClassifier as ABC
 from scipy.io import arff
 import pandas as pd
 import os
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support , mean_squared_error
+
+import  logging
+logger = logging.getLogger('RF Train')
 def classification_dataset(name):
     data = arff.loadarff(name)
     df = pd.DataFrame(data[0])
@@ -14,13 +18,14 @@ def classification_dataset(name):
     class_map = {b"'NB-No Block'": 1, b"'No Block'": 2, b'Block': 0, b'NB-Wait': 3}
     df['Class'] = df['Class'].dropna().map(class_map).astype(int) # encoding
 
-    X = df.drop(['Class',], axis=1)
+    X = df.drop(['Class',], axis=1).values
+
     y = df.iloc[:, -1].values # array
 
     X2 = df.iloc[:, :-3].values
     return X,y
 X,y =  classification_dataset('OBS-Network-DataSet_2_Aug27.arff')
-print(X[:10], y[:10])
+# print( y[:10])
 
 # # Feature Scaling
 # sc = StandardScaler()
@@ -28,30 +33,45 @@ print(X[:10], y[:10])
 # X_test = sc.transform(X_test)
 
 from sklearn.model_selection import train_test_split, GridSearchCV
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
 
-clf = RFC(n_estimators=10, criterion='entropy', random_state=43 , oob_score=True,
+rfc_clf = RFC(n_estimators=100, criterion='gini', random_state=42 , oob_score=False,
           max_features="auto" ,)
+gbc_clf = GBC(random_state=42 , )
+abc_clf = ABC(random_state=42,)
+clf = [rfc_clf,gbc_clf,abc_clf]
+
 params = {"n_estimators":[10,20,50,100,150] ,
           "criterion":["entropy", "gini"],
           "n_jobs":[1,-1],
           "warm_start":[True,False],
           "max_depth" : list(range (10,100,10))
         }
-clf.fit(X_train, y_train)
-# todo Debug , 为啥出错
-# from sklearn.utils.multiclass import type_of_target
 
-# grid = GridSearchCV(cv = 10, estimator=clf , param_grid=params)
+metric = [accuracy_score, mean_squared_error]
+for classifier in clf:
+    classifier.fit(X_train, y_train)
+
+    pred_train = classifier.predict(X_train)
+    pred_test = classifier.predict(X_test)
+    train_acc = accuracy_score(y_train, pred_train)
+    test_acc = accuracy_score(y_test, pred_test)
+    print("The {2} has a train score: {0:.4f}, test score: {1:.4f}".format(train_acc, test_acc,classifier.__class__.__name__))
+
+    train_acc = mean_squared_error(y_train, pred_train)
+    test_acc = mean_squared_error(y_test, pred_test)
+    print("The {2} has a train mse: {0:.4f}, test mse: {1:.4f}".format(train_acc, test_acc,classifier.__class__.__name__))
+    print()
+# from sklearn.utils.multiclass import type_of_target
+#
+logger.log(level=1, msg= "start to fit grid")
+# grid = GridSearchCV(cv = 10, estimator=rfc_clf , param_grid=params)
 # grid.fit(X_train, y_train)
 # sorted(grid.cv_results_.keys())
 
-# pred_train = grid.predict(X_train)
-# pred_test = grid.predict(X_test)
-# from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-# train_acc = accuracy_score(y_train, pred_train)
-# test_acc = accuracy_score(y_test, pred_test)
-# print ("train acc: {0:.2f}, test acc: {1:.2f}".format(train_acc, test_acc))
+grid = rfc_clf
+
+
 # Evaluation
 # 评价方法：大一点的数据集采用10-折交叉验证（10-fold cross validation），小一点的数据集（如200以下）采用留一测试。
 
